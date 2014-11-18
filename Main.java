6c637cf4cae5470492f6123d6a982ce0abe3bfa4
Ben <iofelben+github@gmail.com>
2014-11-17 20:41:15 -0500

    indentation

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
 			System.out.println(
 					new Match<>(new Tuple<>(i % 3, i % 5))
 						.Case(t -> t._1 == 0 && t._2 == 0, 	"FizzBuzz")
-						.Case(t -> t._1 == 0, 				"Fizz")
-						.Case(t -> t._2 == 0, 			    "Buzz")
+						.Case(t -> t._1 == 0,               "Fizz")
+						.Case(t -> t._2 == 0,               "Buzz")
 						.Else(i)
 						.done()
 			)
